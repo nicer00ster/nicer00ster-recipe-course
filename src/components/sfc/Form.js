e3332abf578a5cca1bc8cec8152908d9f868b292
Alex Busch <abusch1013@gmail.com>
2018-07-06 08:05:41 -0400

    Condensed Form.js

--- src/components/sfc/Form.js
+++ src/components/sfc/Form.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom';
 
 const Form = props => (
     <div>
-      <form onSubmit={props.onSubmit} className={`form__${props.className}`}>
-        <div className={`form__${props.className}--title`}>
+      <form onSubmit={props.onSubmit} className="form">
+        <div className="form__title">
           REACT RECIPES!
         </div>
         <h4>{props.subTitle}</h4>
         { props.location.pathname === "/register"
-          ? <div className={`form__${props.className}--username`}>
+          ? <div className="form__username">
               <input onChange={props.handleUsername} required placeholder="Username" type="text" />
             </div>
           : null }
-        <div className={`form__${props.className}--email`}>
+        <div className="form__email">
           <input onChange={props.handleEmail} required placeholder="Email" type="email" />
         </div>
-        <div className={`form__${props.className}--password`}>
+        <div className="form__password">
           <input onChange={props.handlePassword} required placeholder="Password" type="password" />
         </div>
-        <input className={`form__${props.className}--submit`} type="submit" value={props.button} />
+        <input className="form__submit" type="submit" value={props.button} />
         <div>
-          <span className={`form__${props.className}--account`}>{props.message}{' '}<Link to={props.redirect}>{props.link}</Link></span>
+          <span className="form__register">{props.message}{' '}<Link to={props.redirect}>{props.link}</Link></span>
         </div>
       </form>
     </div>
